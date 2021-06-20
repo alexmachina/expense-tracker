7fc4986730a2b17b00925607f4ece3e9d13fc1b9
Alex Alonso <alex.alonso@ae.studio>
2021-06-20 18:02:49 -0300

    Account graphQL crud

--- index.ts
+++ index.ts
@@ -1,13 +1,100 @@
 import dotenv from "dotenv";
-dotenv.config();
-import Account from "./src/models/account";
+dotenv.config({
+  path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env",
+});
 import AccountRepository from "./src/repository/account";
 import client from "./src/repository/client";
-client.connect();
 
-(async function () {
-  const myAccount = await AccountRepository.create(
-    new Account({ title: "Alex Account" })
-  );
-  console.log(myAccount);
-})();
+const { ApolloServer, gql } = require("apollo-server");
+const typeDefs = gql`
+  interface MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+  }
+
+  type Account {
+    id: ID!
+    title: String!
+    Entries: [Entry]
+  }
+
+  type Entry {
+    id: ID!
+    date: String
+    num: String
+    description: String
+    amount: String
+    account: Account
+  }
+
+  type Query {
+    accounts: [Account]
+    account(id: ID!): Account
+  }
+
+  type CreateAccountMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    account: Account
+  }
+
+  input UpdateAccountInput {
+    id: ID!
+    title: String!
+  }
+
+  type Mutation {
+    createAccount(title: String!): CreateAccountMutationResponse
+    updateAccount(input: UpdateAccountInput): CreateAccountMutationResponse
+  }
+
+  input AccountInput {
+    title: String!
+  }
+`;
+
+const resolvers = {
+  MutationResponse: {
+    __resolveType(mutationResponse, context, info) {
+      return null;
+    },
+  },
+  Query: {
+    accounts: async () => {
+      const accounts = await AccountRepository.findAll();
+      return accounts;
+    },
+    account: async (root, args) => {
+      const account = await AccountRepository.findById(args.id);
+      return account;
+    },
+  },
+  Mutation: {
+    createAccount: async (account, args) => {
+      const newAccount = await AccountRepository.create(args);
+      return {
+        code: "200",
+        success: true,
+        message: `Account ${newAccount.title} created`,
+        account: newAccount,
+      };
+    },
+    updateAccount: async (account, args) => {
+      const updatedAccount = await AccountRepository.update(args.input);
+      return {
+        code: "200",
+        success: true,
+        message: `Account ${updatedAccount.id} updated`,
+        account: updatedAccount,
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(async ({ url }) => {
+  await client.connect();
+  console.log(`Server ready at ${url}`);
+});
--- src/repository/account.ts
+++ src/repository/account.ts
@@ -9,5 +9,32 @@ export default class AccountRepository {
     return { ...account, id: result.rows[0].id };
   }
 
+  static async findById(id: number): Promise<Account> {
+    const query = "SELECT id, title FROM accounts WHERE id = $1";
+    const values = [id];
+    const result = await client.query(query, values);
+    return { title: result.rows[0].title, id: result.rows[0].id };
+  }
+
+  static async findAll(): Promise<Account[]> {
+    const query = "SELECT id, title FROM accounts";
+    const result = await client.query(query);
+    return result.rows;
+  }
+
+  static async update(account: Account): Promise<Account> {
+    const query = "UPDATE accounts SET title=$1 WHERE id=$2 RETURNING *";
+    const values = [account.title, account.id];
+    const result = await client.query(query, values);
+    return result.rows[0];
+  }
+
+  static async delete(account: Account): Promise<Account> {
+    const query = "DELETE FROM accounts WHERE id=$1";
+    const values = [account.id];
+    const result = await client.query(query, values);
+    return resul;
+  }
+
   constructor() {}
 }
